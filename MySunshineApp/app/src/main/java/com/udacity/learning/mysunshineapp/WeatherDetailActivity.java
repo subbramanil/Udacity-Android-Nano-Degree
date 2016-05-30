f91bbf44b6ae4854ea5a107ebebd4e617a21af71
Subbramanian Lakshmanan <sxl146830@utdallas.edu>
2016-05-29 19:08:54 -0500

    My Sunshine App: Updated Settings Added share capability

--- MySunshineApp/app/src/main/java/com/udacity/learning/mysunshineapp/WeatherDetailActivity.java
+++ MySunshineApp/app/src/main/java/com/udacity/learning/mysunshineapp/WeatherDetailActivity.java
@@ -1,13 +1,8 @@
 package com.udacity.learning.mysunshineapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.udacity.learning.mysunshineapp.model.WeatherData;
 
 public class WeatherDetailActivity extends AppCompatActivity {
 
@@ -18,23 +13,9 @@ public class WeatherDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_detail);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-        // get the received intent and obtain the weather data passed from the SunshineHomeActivity
-        Intent recdIntent = getIntent();
-        WeatherData weatherData = null;
-        if (recdIntent != null) {
-            weatherData = recdIntent.getParcelableExtra("selected_weather_data");
-            Log.d(TAG, "onCreate: weatherData: " + weatherData);
-            Toast.makeText(getApplicationContext(), weatherData.toString(), Toast.LENGTH_SHORT).show();
-        }
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.detail_container, WeatherDetailActivityFragment.newInstance(weatherData))
-                    .commit();
-        }
     }
 
 }
--- MySunshineApp/app/src/main/java/com/udacity/learning/mysunshineapp/WeatherDetailActivityFragment.java
+++ MySunshineApp/app/src/main/java/com/udacity/learning/mysunshineapp/WeatherDetailActivityFragment.java
@@ -1,13 +1,23 @@
 package com.udacity.learning.mysunshineapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.udacity.learning.mysunshineapp.model.WeatherData;
 
@@ -17,29 +27,25 @@ import com.udacity.learning.mysunshineapp.model.WeatherData;
 public class WeatherDetailActivityFragment extends Fragment {
 
     private static final String TAG = WeatherDetailActivityFragment.class.getSimpleName();
-    private WeatherData data;
+    private static final String HASH_TAG = "My Sunshine App";
+    private WeatherData mWeatherData;
+    private ShareActionProvider mShareActionProvider;
 
     public WeatherDetailActivityFragment() {
-    }
-
-    public static WeatherDetailActivityFragment newInstance(WeatherData data) {
-        WeatherDetailActivityFragment fragment = new WeatherDetailActivityFragment();
-        Bundle args = new Bundle();
-        args.putParcelable("weather_data", data);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Get arguments and fetch the Weather data sent from activity
-        Bundle args = getArguments();
-        Log.d(TAG, "onCreate: args: " + getArguments());
-        if (args != null) {
-            data = args.getParcelable("weather_data");
+        setHasOptionsMenu(true);
+
+        // get the received intent and obtain the weather data passed from the SunshineHomeActivity
+        Intent recdIntent = getActivity().getIntent();
+        if (recdIntent != null) {
+            mWeatherData = recdIntent.getParcelableExtra("selected_weather_data");
+            Log.d(TAG, "onCreate: weatherData: " + mWeatherData);
+            Toast.makeText(getActivity(), mWeatherData.toString(), Toast.LENGTH_SHORT).show();
         }
-        Log.d(TAG, "onCreate: Weather Data: " + data);
     }
 
     @Override
@@ -48,9 +54,71 @@ public class WeatherDetailActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_weather_detail, container, false);
 
         TextView cloudDetailTextView = (TextView) view.findViewById(R.id.cloud_detail);
-        cloudDetailTextView.setText(data.getWeather().get(0).getDescription());
+        cloudDetailTextView.setText(mWeatherData.getWeather().get(0).getDescription());
 
         return view;
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_weather_detail, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.action_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        // like when the user selects a new piece of data they might like to share.
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(TAG, "Share Action Provider is null?");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_show_map) {
+            SharedPreferences sharedPreferences = PreferenceManager
+                    .getDefaultSharedPreferences(getActivity());
+            String location = sharedPreferences.getString(getString(R.string.location_pref_key), "Dallas");
+            Uri geoLocation = Uri.parse("geo:0,0").buildUpon()
+                    .appendQueryParameter("q", location)
+                    .build();
+            showMap(geoLocation);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mWeatherData.getWeather().get(0) + HASH_TAG);
+        return shareIntent;
+    }
+
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, geoLocation);
+        intent.setData(geoLocation);
+        intent.setPackage("com.google.android.apps.maps");
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 }
